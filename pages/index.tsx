3e6566179120f55df375ee82c23eb090c098242e
Nguyen Khai <nguyenvankhai161220@gmail.com>
2022-08-03 15:59:03 +0700

    new push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import 'bootstrap/dist/css/bootstrap.css';
-import '../scripts/index'
+import '../scripts/index.js'
 import Layout from '../componets/Layout';
 
 const Home: NextPage = () => {
   return (
     <Layout title="Index">
       <h1>ddd</h1>
+      <div className='cursor'></div>
+
     </Layout>
   )
 }
